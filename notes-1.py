ec1fc7440325bd590dddf399548e75ac0f9e5e79
maxbildner <39280058+maxbildner@users.noreply.github.com>
2023-06-07 15:59:34 -0400

    update L1 notes

--- notes-1.py
+++ notes-1.py
@@ -272,7 +272,7 @@ print(list1[:-1])  # => ["a", "b", "c"]
 # a[start:stop]  # items start through stop-1
 # a[start:]      # items start through the rest of the array
 # a[:stop]       # items from the beginning through stop-1
-# a[:]           # a copy of the whole array
+# a[:]           # a shallow copy of the whole array
 
 # JS- will get ERROR if you try to use colon to slice an array index
 #   - use array.slice(startIncl, endExcl)
